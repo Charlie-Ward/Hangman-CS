e1ad3434da9526e8a27b8ce416ba641c11916a9e
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2023-08-17 08:58:42 +0100

    added random word selection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #25000 words
+import random
 
 f = open('data.txt', 'r')
 f_content = f.read()
@@ -8,5 +9,10 @@ f_content = f.read()
 #    words[count] = 10
 f_contents = f_content.split(',')
 print(f_contents[0])
+
+
+word_choice = random.choice(f_contents).lstrip()
+print(word_choice)
+print(len(word_choice))
 
 #Random number goes to f_contents to get random word
